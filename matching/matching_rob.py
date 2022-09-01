98890b1a96dbe7f582ecdb878f32ed68321670fc
RobertVenningbo <rove@itu.dk>
2022-09-01 20:50:10 +0200

    whatever kid
    ending up looking at muni's code for insp... am tired :sleepy:

--- matching/matching_rob.py
+++ matching/matching_rob.py
@@ -1,8 +1,17 @@
+from collections import defaultdict
+
+
 def inputthings(): 
     i = input()
     if i.startswith("#"):
         return inputthings()
     return i
+
+def GetOddNumbersAsSet(n):
+    temp = set()
+    for x in range(n):
+        temp.add(x*2+1)
+    return temp
 
 n = int(inputthings().split("=")[1])
 
@@ -10,12 +19,11 @@ people = dict()
 
 for _ in range(2*n):
     y = input().split()
-    people[y[0]] = y[1]
+    people[int(y[0])] = y[1]
     
 input()
 
-proposers = dict()
-non_proposers = dict()
+kms = dict()
 
 for x in range(2*n):
     ratingdict = dict()
@@ -23,23 +31,34 @@ for x in range(2*n):
     int_id = int(y[0])
     temp_dict = dict()
     int_pref = list(map(int,y[1].split(" "))) # e.g. [1: 2,4]
-    if int(y[0]) % 2 == 0:
+    if int_id % 2 == 0:
         for counter, i in enumerate(int_pref):
             ratingdict[i] = counter            
-        non_proposers[y[0]] = ratingdict
+        kms[int_id] = ratingdict
     else:
-        proposers[y[0]] = int_pref
+        kms[int_id] = int_pref
 
-def GetOddNumbers(n):
-    temp = list()
-    for x in range(n):
-        temp.append(x*2+1)
-    return temp
 
-proposer_indexes = GetOddNumbers(n)
+proposer_ids = GetOddNumbersAsSet(n)
 
-print(proposer_indexes)
-print(proposers)
-print(non_proposers)
-# proposers {'1': [2, 4], '3': [4, 2]}
-# non_proposers {'2': {3: 0, 1: 1}, '4': {1: 0, 3: 1}}
+matches = defaultdict(lambda : None)
+
+print(kms)
+
+while(len(proposer_ids) > 0):
+    curr_proposer = proposer_ids.pop()
+    curr_non_proposer = kms[curr_proposer].pop(0)
+    if matches[curr_non_proposer]:
+        if kms[curr_non_proposer][matches[curr_non_proposer]] > kms[curr_non_proposer][curr_proposer]:
+            #comparing current matched proposers rating with asking proposers rating
+            proposer_ids.add(matches[curr_non_proposer])
+            matches[curr_non_proposer] = curr_proposer
+        else:
+            proposer_ids.add(curr_proposer)
+    else:
+        matches[curr_proposer] = curr_non_proposer
+
+for i in range(n):
+    print(people[i*2+1], " + ", people[matches[i*2+1]])
+    
+
