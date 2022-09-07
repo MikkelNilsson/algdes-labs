bf9ab38fb783f7d427996fce8481ea3a9c729838
RobertVenningbo <rove@itu.dk>
2022-09-07 11:56:25 +0200

    alt det gode du ved

--- matching/alt_det_gode_matching.py
+++ matching/alt_det_gode_matching.py
@@ -4,8 +4,6 @@ def input_command():
         return input_command()
     return i
 
-
-
 n = int(input_command().split("=")[1])
 
 id_people = dict()
@@ -24,23 +22,22 @@ rankings = dict()
 
 for _ in range(n*2):
     number_pref = input().split(": ")
-    pref = map(int, number_pref[1].split(" "))
     if int(number_pref[0]) % 2 == 0:
-        tmp = dict()
-        for i, m in enumerate(pref):
-            tmp[m] = i
-        rankings[int(number_pref[0])] = tmp
+        rankings[int(number_pref[0])] = dict()
+        pref_order = number_pref[1].split(" ")
+        for i in range(n):
+            rankings[int(number_pref[0])][int(pref_order[i])] = i
     else:
-        rankings[int(number_pref[0])] = list(pref)
+        rankings[int(number_pref[0])] = number_pref[1].split(" ")
 
 suiters = set([i*2 + 1 for i in range(n)])
 matches = dict()
 
 
 while suiters:
-    m = suiters.pop()
-    w = rankings[m].pop(0)
-    if w in m:
+    m = int(suiters.pop())
+    w = int(rankings[m].pop(0))
+    if w in matches:
         if rankings[w][matches[w]] > rankings[w][m]:
             suiters.add(matches[w])
             matches[w] = m
@@ -52,7 +49,5 @@ while suiters:
         matches[m] = w
 
 
-for i in range(n):
-    print(id_people[i * 2 + 1], "--", id_people[matches[i * 2 + 1]])
-    
+[print(id_people[i * 2 + 1], "--", id_people[matches[i * 2 + 1]]) for i in range(n)]
 
