ae2e30eb740ce3bba7fe8808653f265a61941264
chtof <chtof@itu.dk>
2022-09-19 17:59:39 +0200

    added sorting of lists by x coordinate

--- closest-points/muni-cp.py
+++ closest-points/muni-cp.py
@@ -15,7 +15,7 @@ def getarray():
         d = 1
         while all[d] != "NODE_COORD_SECTION":
             d += 1
-        all = all[d:]
+        all = all[d+1:]
     while all[-1] == "EOF" or all[-1] == '':
         all = all[:-1]
     return all
@@ -26,11 +26,21 @@ ctn = dict()
 cl = list()
 for v in input:
     vl = v.split(maxsplit=1)
+    print(vl)
     n = vl[0]
     c = tuple(map(float, vl[1].split()))
     ctn[c] = n
     cl.append(c)
 
+cl.sort(key=lambda x:x[0])
+firstHalf, secondHalf = cl[:(len(cl)//2)],cl[(len(cl)//2):]
+print("Below is the first half sorted:")
+print(firstHalf)
+print("")
+print("Below is the second half sorted:")
+print(secondHalf)
+print("")
 
-print(ctn)
+
+
 
