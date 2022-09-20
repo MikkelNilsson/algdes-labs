315abdaeac4e2559cdd893bb2bfefcc7f817f2b1
Oskar Jensen <osje@itu.dk>
2022-09-20 08:46:52 +0200

    Tæt på at virke, giver forkerte svar nogengange

--- closest-points/src/closest_pair_oskar.py
+++ closest-points/src/closest_pair_oskar.py
@@ -1,4 +1,4 @@
-from cmath import sqrt
+from math import sqrt
 import sys
 
 def readall():
@@ -23,30 +23,48 @@ def getarray():
 
 def rec(list):
     if(len(list) == 2):
-        (x1, y1) = list[0]
-        (x2, y2) = list[1]
-        return euclid(x1, y1, x2, y2)
+        return euclid(list[0], list[1])
     elif(len(list) < 2):
-        return 100000000
+        return -1
     else:
         firstHalf, secondHalf = list[:(len(list)//2)],list[(len(list)//2):]
-        bestLeft = int(rec(firstHalf))
-        bestRight = int(rec(secondHalf))
-        #Problem er complex tal, dem med E i 
-        best = min(bestLeft, bestRight)
+        bestLeft = rec(firstHalf)
+        bestRight = rec(secondHalf)
+        if (bestLeft >= 0 and bestRight >= 0):
+            best = min(bestLeft, bestRight)
+        else:
+            best = max(bestLeft, bestRight)
+        (l, r) = createYList(firstHalf, secondHalf, best)
+        l.sort(key=lambda x:x[1])
+        r.sort(key=lambda x:x[1])
+
+        #TODO Undersøg om alle muligheder bliver dækket
+        for i in range(min(len(l), len(r))-1):
+            e1 = euclid(l[i], r[i])
+            e2 = euclid(l[i], r[i+1])
+            e3 = euclid(l[i+1], r[i])
+            best = min(best, e1, e2, e3)
+        if(len(l) > 0 and len(r) > 0):
+            best = min(best, euclid(l[len(l)-1], r[len(r)-1]))
+            if(len(l) < len(r)):
+                best = min(best, euclid(l[len(l)-1], r[len(r)-2]))
+            elif(len(l) > len(r)):
+                best = min(best, euclid(l[len(l)-2], r[len(r)-1]))
+
         return best
 
-def euclid(x1, y1, x2, y2):
+def euclid(p1, p2):
+    (x1, y1) = p1
+    (x2, y2) = p2
     return sqrt((x2-x1)**2+(y2-y1)**2)
 
 def createYList(L1, L2, dist):
     newLeft  = []
     newRight = []
 
-
     len1 = len(L1)
     len2 = len(L2)
-    mid = L1[len1-1][0] + (euclid((L1[len1-1][0]), 0, L2[0][0], 0))/2
+    mid = L1[len1-1][0] + (euclid(((L1[len1-1][0]), 0), (L2[0][0], 0)))/2
     for i in range(len1):
         if (abs( mid - L1[len1-1-i][0]) > dist):
             continue
@@ -76,12 +94,6 @@ for v in input:
 cl.sort(key=lambda x:x[0])
 
 print(str(rec(cl)))
-
-firstHalf, secondHalf = cl[:(len(cl)//2)],cl[(len(cl)//2):]
-
-(l, r) = createYList(firstHalf, secondHalf, 1000)
-print(l)
-print(r)
 
 
 
