84925a09903fda8ca4194c08554861aced813b64
Mikkel Munkholm Blak Nilsson <muni@itu.dk>
2022-09-29 14:35:37 +0200

    gorilla parser

---
+++ gorilla/Parser.py
@@ -0,0 +1,25 @@
+
+def inputProtein():
+    s = input().split()[0].strip(">")
+    proteinstrings = dict()
+    p = ""
+    inp = input()
+    try:
+        while True:
+            if ">" not in inp:
+                p += inp
+                inp = input()
+            else:
+                proteinstrings[s] = p
+                p = ""
+                s = inp.split()[0].strip(">")
+                inp = input()
+
+    except EOFError:
+        proteinstrings[s] = p
+        return proteinstrings
+
+
+print(inputProtein())
+
+
